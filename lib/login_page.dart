d598027539807813cec7a00efee7a825a9f15876
k4droid3 <k4droid3@gmail.com>
2020-03-03 15:30:36 +0530

    add login button in login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -96,6 +96,23 @@ class LoginScreen extends StatelessWidget {
                     ],
                   ),
                 ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.white54,
+                  ),
+                  child: FlatButton(
+                    onPressed: () {
+                      print('someone Logged in!');
+                    },
+                    child: Text(
+                      "Log In",
+                      style: TextStyle(
+                        color: Colors.teal[700],
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
           ),
