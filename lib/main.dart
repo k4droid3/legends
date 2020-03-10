8f646da921bdf6b19318388db0ccded85c6f0217
k4droid3 <k4droid3@gmail.com>
2020-03-10 22:45:26 +0530

    working dart implementation with flutter UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import "dart:io";
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+import 'package:flutter_xmpp/flutter_xmpp.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,6 +15,70 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String incomingMessage = "incoming Empty...";
   String outgoingMessage = "outgoing Empty...";
+
+//  String name = "";
+//  String message = "";
+//  String id = "";
+//  String reply = "";
+
+  FlutterXmpp flutterXmpp;
+
+  @override
+  void initState() {
+    super.initState();
+    initXmpp();
+  }
+
+  @override
+  void dispose() async {
+    await flutterXmpp.stop();
+    super.dispose();
+  }
+
+  Future<void> initXmpp() async {
+    var auth = {
+      "user_jid": "bot@legends.com",
+      "password": "qwerty",
+      "host": "172.25.173.138",
+      "port": 5222
+    };
+
+    flutterXmpp = new FlutterXmpp(auth);
+    print("AUTHENTICATED O_O");
+
+    await flutterXmpp.login();
+
+    await flutterXmpp.start(_onReceive, _onError);
+
+    sleep(const Duration(seconds: 2));
+
+    print(await flutterXmpp.currentState());
+  }
+
+  void _onReceive(dynamic event) {
+    print(event);
+    if (event["type"] == "incoming") {
+      setState(() {
+        incomingMessage = event['body'];
+      });
+    } else {
+      setState(() {
+        outgoingMessage = event['body'];
+      });
+    }
+  }
+
+  void _onError(Object error) {
+    print(error);
+  }
+
+  void giveMessage(String message) async {
+    await flutterXmpp.sendMessage("k4droid3@legends.com", message, "loda");
+    setState(() {
+      outgoingMessage = message;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -38,7 +107,7 @@ class _MyAppState extends State<MyApp> {
                 autocorrect: false,
                 onSubmitted: (String message) {
                   setState(() {
-                    outgoingMessage = message;
+                    giveMessage(message);
                   });
                 },
               ),
