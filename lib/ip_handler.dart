9a8b74b4148a6b596a28e417485b475a4a2a67c3
k4droid3 <k4droid3@gmail.com>
2020-03-13 20:12:46 +0530

    edit ip_handler for correct pastebin URL

--- lib/ip_handler.dart
+++ lib/ip_handler.dart
@@ -1,6 +1,6 @@
 import 'package:http/http.dart';
 
-final String pasteBinURL = "https://pastebin.com/raw/eDzY0LSq";
+final String pasteBinURL = "https://pastebin.com/raw/eKDDy5EW";
 
 Future<String> getIP() async {
   final response = await get(pasteBinURL);
