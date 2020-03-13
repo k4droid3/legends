eee38faf20cd31764111cddfccf062b4d0657f03
k4droid3 <k4droid3@gmail.com>
2020-03-13 18:51:49 +0530

    added the UI for incoming and outgoing messages in the listview but listview is not ordered correctly

--- lib/group_chat_screen.dart
+++ lib/group_chat_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'dart:math';
+import 'dart:async';
 import 'package:legends/Xmpp_handler.dart';
+import 'package:legends/login_screen.dart';
+
+StreamController<List<MyMessage>> databaseController =
+    StreamController<List<MyMessage>>();
+Stream<List<MyMessage>> stream;
+BuildContext mainBuildContext;
+List<MyMessage> messageDatabase = [];
 
 class GroupChatScreen extends StatefulWidget {
   static const String id = "groupchatscreen";
-
   @override
   _GroupChatScreenState createState() => _GroupChatScreenState();
 }
@@ -17,6 +25,7 @@ class _GroupChatScreenState extends State<GroupChatScreen> {
 
   @override
   void initState() {
+    stream = databaseController.stream.asBroadcastStream();
     textEditingController = TextEditingController();
     super.initState();
   }
@@ -59,8 +68,11 @@ class _GroupChatScreenState extends State<GroupChatScreen> {
               ),
             ),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
+                Expanded(
+                  child: MessagesStream(),
+                ),
                 SafeArea(
                   maintainBottomViewPadding: true,
                   bottom: true,
@@ -86,7 +98,7 @@ class _GroupChatScreenState extends State<GroupChatScreen> {
                                 color: Colors.white54,
                               ),
                               filled: false,
-                              fillColor: Colors.teal[500],
+                              fillColor: Colors.brown[500],
                               contentPadding: EdgeInsets.symmetric(
                                   vertical: 5.0, horizontal: 20.0),
                               border: OutlineInputBorder(
@@ -96,14 +108,18 @@ class _GroupChatScreenState extends State<GroupChatScreen> {
                               enabledBorder: OutlineInputBorder(
                                 borderRadius:
                                     BorderRadius.all(Radius.circular(32.0)),
-                                borderSide:
-                                    BorderSide(color: Colors.teal, width: 0.0),
+                                borderSide: BorderSide(
+                                    color: Colors.brown,
+                                    width: 0.0,
+                                    style: BorderStyle.solid),
                               ),
                               focusedBorder: OutlineInputBorder(
                                 borderRadius:
                                     BorderRadius.all(Radius.circular(32.0)),
-                                borderSide:
-                                    BorderSide(color: Colors.teal, width: 0.0),
+                                borderSide: BorderSide(
+                                    color: Colors.brown,
+                                    width: 0.0,
+                                    style: BorderStyle.solid),
                               ),
                             ),
                             style: TextStyle(color: Colors.white),
@@ -153,6 +169,93 @@ class _GroupChatScreenState extends State<GroupChatScreen> {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MessagesStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: stream,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<MessageBubble> MessageBubbles = [];
+          final messages = snapshot.data;
+          for (var message in messages) {
+            final messageText = message.data;
+            final messageSender = message.from;
+
+            final currentUser = loggedInUser;
+
+            final messageBubble = MessageBubble(
+              sender: messageSender,
+              text: messageText,
+              isMe: currentUser == messageSender,
+            );
+            MessageBubbles.add(messageBubble);
+          }
+          return ListView(
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children: MessageBubbles,
+          );
+        } else {
+          return Column(
+            children: <Widget>[],
+          );
+        }
+      },
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble({this.sender, this.text, this.isMe});
+
+  final String sender;
+  final String text;
+  final bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            sender,
+            style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.white70,
+                fontWeight: FontWeight.bold),
+          ),
+          Material(
+            borderRadius: isMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0))
+                : BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0)),
+            elevation: 5.0,
+            color: isMe ? Colors.brown : Colors.tealAccent,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: isMe ? Colors.white : Colors.black87,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
